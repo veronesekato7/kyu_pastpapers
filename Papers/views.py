4484361fcbc66290508a833e94cc638e7d4ed251
veronesekato7 <veronesekato7@gmail.com>
2025-07-11 15:28:26 +0300

    created a form , and view for universities,listing all universities and adding

--- Papers/views.py
+++ Papers/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UniversityForm, DepartmentForm, CourseForm, YearForm, CourseUnitForm, PaperForm
+from .models import University, Department, Course, Year, CourseUnit, Paper
 
-# Create your views here.
+# Home Page
+def home(request):
+    return render(request, 'home.html')
+
+# Add and list universities
+def university_list_create(request):
+    if request.method == 'POST':
+        form = UniversityForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('university_list')
+    else:
+        form = UniversityForm()
+    
+    universities = University.objects.all()
+    return render(request, 'university_list.html', {'form': form, 'universities': universities})
