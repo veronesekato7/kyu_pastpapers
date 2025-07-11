22451728c8405c85ab43bb7979cc14e364cec316
veronesekato7 <veronesekato7@gmail.com>
2025-07-11 15:05:18 +0300

    created models of papers and made migrations,registerd the modelss in project

--- Papers/admin.py
+++ Papers/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from models import University, Department, Course, Year, CourseUnit, Paper
 
 # Register your models here.
+admin.site.register(University)
+admin.site.register(Department)
+admin.site.register(Course)
+admin.site.register(Year)
+admin.site.register(CourseUnit)
+admin.site.register(Paper)
--- Papers/models.py
+++ Papers/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
 # Create your models here.
+#University name top
+class University(models.Model):
+    name = models.CharField(max_length=200)
+    def __str__(self):
+        return self.name
+    
+ #Department under university second   
+class Department(models.Model):
+    name = models.ForeignKey(University,on_delete = models.CASCADE,related_name='departments')
+    def __str__(self):
+        return f"{self.name} ({self.university.name})"
+
+#Course under university third    
+class Course(models.Model):
+    name = models.CharField(max_length=200)
+    department = models.ForeignKey(Department, on_delete = models.CASCADE,related_name='courses')
+    def __str__(self):
+        return f"{self.name} - {self.department.name}"
+
+#Year under course fourth
+class Year(models.Model):
+    year_number = models.PositiveSmallIntegerField()  # e.g., 1 for Year 1, 2 for Year 2
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='years')
+
+    def __str__(self):
+        return f"Year {self.year_number} - {self.course.name}"
+
+
+#course unit under year
+class CourseUnit(models.Model):
+    name = models.CharField(max_length=200)
+    year = models.ForeignKey(Year, on_delete=models.CASCADE, related_name='course_units')
+
+    def __str__(self):
+        return f"{self.name} - {self.year}"
+
+#paper under course unit with upload in pdf
+class Paper(models.Model):
+    title = models.CharField(max_length=200)
+    course_unit = models.ForeignKey(CourseUnit, on_delete=models.CASCADE, related_name='papers')
+    file = models.FileField(upload_to='papers/')  # optional: attach PDF or document
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title} - {self.course_unit}"
