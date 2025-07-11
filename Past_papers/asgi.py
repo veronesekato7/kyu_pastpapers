7199d5d4771092bafd2eec47ea1c6e8de67d8559
veronesekato7 <veronesekato7@gmail.com>
2025-07-11 14:21:04 +0300

    created django project

---
+++ Past_papers/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Past_papers project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Past_papers.settings')
+
+application = get_asgi_application()
---
+++ Past_papers/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Past_papers project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Past_papers.settings')
+
+application = get_wsgi_application()
